976044afc935292ae54248cb690436156601a741
Arthur Leonardo <arthurleonardo801@hotmail.com>
2021-08-15 22:34:19 -0300

    Criado_uma_View_Home

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jobs_net.Apresentacao;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace jobs_net.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/")]
+        public HomeView Get()
+        {
+           return new HomeView(); 
+        }
+    }
+}
